7355f11fb0fdef73b78170bc4143371f4148aac6
seziumm <matteoberardinetti2801@gmail.com>
2025-02-14 20:13:04 +0100

    .

--- nixos/modules/boot.nix
+++ nixos/modules/boot.nix
@@ -1,6 +1,12 @@
 {
-  boot.loader.grub.enable = true;
-  boot.loader.grub.device = "nodev";
-  boot.loader.grub.efiSupport = true;
-  boot.loader.grub.configurationLimit = 5; 
+  # 
+  # boot.loader.grub.enable = true;
+  # boot.loader.grub.device = "/dev/sda";
+  # boot.loader.grub.efiSupport = true;
+  # boot.loader.grub.configurationLimit = 5; 
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.systemd-boot.configurationLimit = 5; 
+
 }
