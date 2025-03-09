1bc1ac45b14606886d5acf5275c1d4d4cbe27c75
seziumm <matteoberardinetti2801@gmail.com>
2025-03-09 14:09:48 +0100

    animations

---
+++ home-manager/modules/scripts/animation.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+HYPRCTL="hyprctl keyword animations:enabled"
+
+CURRENT_STATE=$(hyprctl getoption animations:enabled | awk 'NR==1 {print $NF}')
+
+if [ "$CURRENT_STATE" = "1" ]; then
+  $HYPRCTL 0
+else
+  $HYPRCTL 1
+fi
