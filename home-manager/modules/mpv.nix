7bf3f723826a859aabdcc75a5f4cc9dcf9c63a00
seziumm <matteoberardinetti2801@gmail.com>
2025-05-07 10:51:48 +0200

    .

--- home-manager/modules/mpv.nix
+++ home-manager/modules/mpv.nix
@@ -9,6 +9,8 @@
 # Scroll tra le immagini (come h/l in Vim)
       h = "playlist-prev";
       l = "playlist-next";
+      r = "cycle_values video-rotate 0 90 180 270";
+
 
 # Movimenti rapidi avanti/indietro (come j/k in Vim) +, - seconds to the video
       j = "seek -5";
