94614145734f9dd7d9aa481cee8d85377e96b65e
seziumm <matteoberardinetti2801@gmail.com>
2025-06-02 21:11:46 +0200

    fix fzf color

--- home-manager/modules/scripts/vif.sh
+++ home-manager/modules/scripts/vif.sh
@@ -9,9 +9,7 @@ selected_files=$(fd . "$SEARCH_PATH" \
         --preview 'bat -n --color=always --line-range=:500 {} 2>/dev/null || echo "Error previewing file"' \
         --height=70% \
         --layout=default \
-        --bind 'ctrl-J:down,ctrl-K:up' \
-        --color 'fg:#ebdbb2,bg:#272727,hl:#fabc2e,fg+:#ebdbb2,bg+:#655b53,hl+:#fabc2e' \
-        --color 'info:#83a597,prompt:#a89983,spinner:#fabc2e,pointer:#83a597,marker:#d65c0d,header:#928373')
+        --bind 'ctrl-J:down,ctrl-K:up')
 
 # Esci se non selezionato nulla
 if [[ -z "$selected_files" ]]; then
--- home-manager/modules/scripts/zvif.sh
+++ home-manager/modules/scripts/zvif.sh
@@ -9,9 +9,7 @@ selected_dirs=$(fd --type d . "$SEARCH_PATH" \
         --preview 'tree -C {} | head -100' \
         --height=70% \
         --layout=default \
-        --bind 'ctrl-J:down,ctrl-K:up' \
-        --color 'fg:#ebdbb2,bg:#272727,hl:#fabc2e,fg+:#ebdbb2,bg+:#655b53,hl+:#fabc2e' \
-        --color 'info:#83a597,prompt:#a89983,spinner:#fabc2e,pointer:#83a597,marker:#d65c0d,header:#928373')
+        --bind 'ctrl-J:down,ctrl-K:up')
 
 if [[ -z "$selected_dirs" ]]; then
   echo "Nessuna cartella selezionata."
