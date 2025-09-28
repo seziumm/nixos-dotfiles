e278336298998c8bd576b33f65a56a203adfebaa
seziumm <matteoberardinetti2801@gmail.com>
2025-09-28 12:56:20 +0200

    local send

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -11,23 +11,30 @@
     ./printer.nix
     ../../nixos/modules/fonts.nix
   ];
-  nixpkgs.config = { allowBroken = true; allowUnfree = true; }
-  ;
 
-  environment.systemPackages = [ pkgs.nvidia-docker pkgs.home-manager ];
+  nixpkgs.config = {
+    allowBroken = true;
+    allowUnfree = true;
+  };
+
+  environment.systemPackages = [
+    pkgs.nvidia-docker
+    pkgs.home-manager
+  ];
 
   networking.hostName = hostname;
 
+  # 🔑 apre le porte usate da LocalSend
+  networking.firewall.allowedTCPPorts = [ 53317 ];
+  networking.firewall.allowedUDPPorts = [ 53317 ];
+
+  # 🔑 utile per il discovery (LocalSend usa broadcast/mDNS)
+  services.avahi.enable = true;
+  services.avahi.nssmdns = true;
+
   system.stateVersion = stateVersion;
 
-# used in github desktop
+  # usato in github desktop
   services.gnome.gnome-keyring.enable = true;
-
   programs.seahorse.enable = true;  # Optional GUI to manage keys
-
-
-
-
-
 }
-
